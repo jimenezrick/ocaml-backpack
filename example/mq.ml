6e7b1da34535a9f4de92288d57a24ecf18210933
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-03-26 01:08:15 +0200

    Add example using IPC

---
+++ example/mq.ml
@@ -0,0 +1,42 @@
+open Backpack
+
+type msg = Msg of int
+
+let send mq n =
+    let s = Marshal.to_string (Msg n) [] in
+    Unix.Mqueue.send mq s 0 (String.length s) 6
+
+let recv mq =
+    let s     = String.create (Unix.Mqueue.msgsize_max ()) in
+    ignore (Unix.Mqueue.receive mq s 0 (String.length s));
+    let Msg n = Marshal.from_string s 0 in
+    n
+
+let create name =
+    let open Unix.Mqueue in
+    create_mq name [O_CREAT; O_RDWR] 0o666 Mq_defs
+
+let send_loop () =
+    let mq = create "/foo" in
+    let rec f mq n =
+        send mq n;
+        Printf.printf "Msg sent %d\n%!" n;
+        f mq (n + 1)
+    in f mq 0
+
+let recv_loop () =
+    let mq = create "/foo" in
+    let rec f mq =
+        let n = recv mq in
+        Printf.printf "Msg received %d\n%!" n;
+        Unix.sleep 1;
+        f mq
+    in f mq
+
+let usage () = Printf.fprintf stderr "Usage: %s -s|-r\n" Sys.argv.(0)
+
+let () =
+    match Sys.argv with
+    | [|_; "-s"|] -> send_loop ()
+    | [|_; "-r"|] -> recv_loop ()
+    | _           -> usage ()
